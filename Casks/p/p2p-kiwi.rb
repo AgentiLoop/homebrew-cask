197afcb57f9a1922d576cd67799f8c80cc00c40f
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2026-09-15 19:23:44 +0000

    p2p-kiwi 2.0.1

--- Casks/p/p2p-kiwi.rb
+++ Casks/p/p2p-kiwi.rb
@@ -2,11 +2,11 @@ cask "p2p-kiwi" do
   arch arm: "arm64", intel: "x86_64"
   url_end = on_system_conditional macos: "universal.dmg", linux: "#{arch}.AppImage"
 
-  version "1.0.0"
-  sha256 arm:          "18d6082cdcf073e0842d82d9235882454ed42325868547da56700a4542703b6f",
-         intel:        "18d6082cdcf073e0842d82d9235882454ed42325868547da56700a4542703b6f",
-         arm64_linux:  "ee1db1cfd19c508b7c02dd4fd4c3334a9bbc8377e093c0283c79388871983bb9",
-         x86_64_linux: "7bf9b8e2b4e200cc567bdfa570a46e377c355090621125bc1ce1a50671f60386"
+  version "2.0.1"
+  sha256 arm:          "9dea64a41ffa74cf17f37819d35b3c5cd6c5d26ffbd34b3dd545c1b66bc182c4",
+         intel:        "9dea64a41ffa74cf17f37819d35b3c5cd6c5d26ffbd34b3dd545c1b66bc182c4",
+         arm64_linux:  "4500007a5f585cc6fa62f8a517825820ecf383d5633bbb276081c2dadae55cd2",
+         x86_64_linux: "2065de4502160095d49c6734e000c603c9729adaa6b47ac240d4a2b2d7685cff"
 
   on_macos do
     depends_on macos: :ventura
